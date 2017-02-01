63d992b4f69829aca1f07c584890092395bc6257
Whitney Meulink <wmeulink15@wou.edu>
2017-02-01 11:03:53 -0800

    Adding Fake News Project

---
+++ Milestone3/FakeNews/FakeNews/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(FakeNews.Startup))]
+namespace FakeNews
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
