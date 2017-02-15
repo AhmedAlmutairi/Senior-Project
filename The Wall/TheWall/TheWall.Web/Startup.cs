d3521efeca0a8cefc44fff5ed96b93b4762c4da3
RobBalsley <rbalsley10@wou.edu>
2017-02-15 11:05:14 -0800

    Added skeleton VS project and updated the database diagram.

---
+++ The Wall/TheWall/TheWall.Web/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(TheWall.Web.Startup))]
+namespace TheWall.Web
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
