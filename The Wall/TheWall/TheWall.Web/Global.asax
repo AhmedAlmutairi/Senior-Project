e48d0e434f1e5b03f34fb7bf048359a480fbaef6
RobBalsley <rbalsley10@wou.edu>
2017-02-21 12:26:51 -0800

    Updated the name of the VS myWall project.

--- The Wall/TheWall/TheWall.Web/Global.asax
+++
@@ -1,1 +0,0 @@
-﻿<%@ Application Codebehind="Global.asax.cs" Inherits="TheWall.Web.MvcApplication" Language="C#" %>
---
+++ myWall/myWall/myWall.Model/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace myWall.Model
+{
+    public class Class1
+    {
+    }
+}
