3328e69f1ba68a83b2a748a990d97dce599984ab
CS460almutairi <aalmutairi15@wou.edu>
2017-02-24 22:13:48 -0800

    added new project & solve data base problem

---
+++ Sprint1/myWall/myWall/Controllers/HomeController.cs
@@ -0,0 +1,58 @@
+﻿using myWall.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace myWall.Controllers
+{
+    public class HomeController : Controller
+    {
+
+        private MyWallContext db = new MyWallContext();
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public ActionResult CreateWall()
+        {
+
+            return View();
+        }
+
+        [HttpPost]
+
+        public ActionResult CreateWall(Wall wall)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Walls.Add(wall);
+
+                db.SaveChanges();
+
+                return RedirectToAction("Index");
+
+            }
+
+            return View(wall);
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
---
+++ Sprint1/myWall/myWall/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(myWall.Startup))]
+namespace myWall
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
