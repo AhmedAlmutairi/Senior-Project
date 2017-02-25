506b353a90dc6f24be3248ee061ae888771c17eb
CS460almutairi <aalmutairi15@wou.edu>
2017-02-24 20:44:31 -0800

    deleted

--- Sprint1/CreateDB/create DB.sql
+++
@@ -1,4 +0,0 @@
-﻿CREATE DATABASE [MyWallDB] ON
-PRIMARY (NAME = 'MyWallDB', FILENAME = 'C:\Users\Ahmed\Documents\engOrg\engorg\Sprint1\CreateDB\MyWallDB.mdf')
-LOG ON (NAME = 'MyWallDB_log', FILENAME = 'C:\Users\Ahmed\Documents\engOrg\engorg\Sprint1\CreateDB\MyWallDB_log.ldf')
-GO
