21bbdd4024a3c1c2107ea56e08b9a95c993846b1
CS460almutairi <aalmutairi15@wou.edu>
2017-02-28 10:49:16 -0800

    solve data base problem

---
+++ Sprint1/myWall/myWall/Repositories/ContentRepository.cs
@@ -0,0 +1,45 @@
+﻿using myWall.Models;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Web;
+using myWall.ViewModel;
+
+namespace myWall.Repositories
+{
+    public class ContentRepository
+    {
+        private readonly MyWallContext db = new MyWallContext();
+        public int myWall(HttpPostedFileBase file, ContentViewModel contentViewModel)
+        {
+            contentViewModel.Image = ConvertToBytes(file);
+            var Content = new Post
+            {
+                Title = contentViewModel.Title,
+                Description = contentViewModel.Description,
+                Contents = contentViewModel.Contents,
+                Image = contentViewModel.Image
+            };
+            db.Posts.Add(Content);
+            int i = db.SaveChanges();
+            if (i == 1)
+            {
+                return 1;
+            }
+            else
+            {
+                return 0;
+            }
+
+        }
+
+        public byte[] ConvertToBytes(HttpPostedFileBase image)
+        {
+            byte[] imageBytes = null;
+            BinaryReader reader = new BinaryReader(image.InputStream);
+            imageBytes = reader.ReadBytes((int)image.ContentLength);
+            return imageBytes;
+        }
+    }
+}
