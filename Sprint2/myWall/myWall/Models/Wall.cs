954183e4f1aa58a6cb622bb852c6cb3bf68a4eda
CS460almutairi <aalmutairi15@wou.edu>
2017-03-14 19:34:31 -0700

    added upload, download files

---
+++ Sprint2/myWall/myWall/Models/Wall.cs
@@ -0,0 +1,35 @@
+namespace myWall.Models
+{
+    using System;
+    using System.Collections.Generic;
+    using System.ComponentModel.DataAnnotations;
+    using System.ComponentModel.DataAnnotations.Schema;
+    using System.Data.Entity.Spatial;
+
+    [Table("Wall")]
+    public partial class Wall
+    {
+        [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2214:DoNotCallOverridableMethodsInConstructors")]
+        public Wall()
+        {
+            Posts = new HashSet<Post>();
+            Groups = new HashSet<Group>();
+        }
+
+        public int Id { get; set; }
+
+        [Required]
+        [StringLength(50)]
+        public string Name { get; set; }
+
+        [Required]
+        [StringLength(128)]
+        public string UserId { get; set; }
+
+        [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2227:CollectionPropertiesShouldBeReadOnly")]
+        public virtual ICollection<Post> Posts { get; set; }
+
+        [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2227:CollectionPropertiesShouldBeReadOnly")]
+        public virtual ICollection<Group> Groups { get; set; }
+    }
+}
