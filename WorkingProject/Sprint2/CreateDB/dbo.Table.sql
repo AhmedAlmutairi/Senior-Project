6e64bc159dce2e25b2600f2f3f0ed11196f00ee2
Whitney Meulink <wmeulink15@wou.edu>
2017-04-10 15:33:32 -0700

    adding work

---
+++ WorkingProject/Sprint2/CreateDB/dbo.Table.sql
@@ -0,0 +1,9 @@
+﻿CREATE TABLE [dbo].[Questions]
+(
+	[id] INT NOT NULL PRIMARY KEY, 
+    [Question] NCHAR(10) NULL, 
+    [Answer] NCHAR(10) NULL, 
+    [Hint] NCHAR(10) NULL, 
+    [Date] NCHAR(10) NULL, 
+    [Email] NCHAR(10) NULL
+)
