d03a935dfefdcc88a6999ec34d10523db0675525
DESKTOP-SVQ8TMB\torpy <pdwalla17@gmail.com>
2017-04-13 11:51:19 -0700

    Added NUnit and MvcRouteTester packages

---
+++ myWallOfficial/myWall/myWall.Tests/Controllers/TestBaseRoutes.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Mvc;
+using System.Web.Routing;
+using NUnit.Framework;
+using MvcRouteTester;
+
+namespace myWall.Tests.Controllers
+{
+    public class TestBaseRoutes
+    {
+        public RouteCollection Routes;
+
+        [SetUp]
+        public void Setup()
+        {
+            Routes = RouteTable.Routes;
+            myWall.RouteConfig.RegisterRoutes(Routes);
+
+            RouteAssert.UseAssertEngine(new NunitAssertEngine());
+        }
+
+        [TearDown]
+        public void TearDown()
+        {
+            RouteTable.Routes.Clear();
+        }
+    }
+}
