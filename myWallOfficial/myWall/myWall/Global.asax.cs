5bd25c07944d709c3849b2c37285233f91a308b7
CS460almutairi <aalmutairi15@wou.edu>
2017-05-01 15:08:58 -0700

    added changes to chat

--- myWallOfficial/myWall/myWall/Global.asax.cs
+++ myWallOfficial/myWall/myWall/Global.asax.cs
@@ -17,6 +17,8 @@ namespace myWall
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
             BundleConfig.RegisterBundles(BundleTable.Bundles);
-        }
+           // RouteTable.Routes.MapHubs();
+            
+    }
     }
 }
--- myWallOfficial/myWall/myWall/Models/IdentityModels.cs
+++ myWallOfficial/myWall/myWall/Models/IdentityModels.cs
@@ -89,7 +89,7 @@ namespace myWall.Models
 
         public ApplicationDbContext()
 
-            : base("SQLAzureConnection", throwIfV1Schema: false)
+           : base("SQLAzureConnection", throwIfV1Schema: false)
         //SQLAzureConnection
 
         //  : base("MyWallContext", throwIfV1Schema: false)
