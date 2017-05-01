f0f7058ae188d7714474d6fce359afe5d4586a5c
CS460almutairi <aalmutairi15@wou.edu>
2017-05-01 15:31:47 -0700

    added changes to chat

--- myWallOfficial/myWall/myWall/Controllers/HomeController.cs
+++ myWallOfficial/myWall/myWall/Controllers/HomeController.cs
@@ -403,7 +403,7 @@ namespace myWall.Controllers
 
          public FileResult DownloadFile(string fileName)
          {
-             var filepath = System.IO.Path.Combine(Server.MapPath("~/Files/"), fileName);
+             var filepath = System.IO.Path.Combine(Server.MapPath("/site/wwwroot/Files/"), fileName);
              return File(filepath, MimeMapping.GetMimeMapping(filepath), fileName);
          }
 
