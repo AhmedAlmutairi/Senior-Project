ca74630b888f4da52661b57241817d648a80a6c3
Whitney Meulink <wmeulink15@wou.edu>
2017-05-10 15:49:40 -0700

    reseting

---
+++ myWallOfficial/myWall/myWall/Hubs/DrawHub.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Microsoft.AspNet.SignalR;
+
+namespace myWall
+{
+        public class DrawHub : Hub
+        {
+            public DrawHub()
+            {
+
+            }
+            public void DrawSomeThings(int x, int y, int z, string w)
+            {
+                Clients.Others.draw(x, y, z, w);
+            }
+
+            public void ClearCanvas()
+            {
+                Clients.All.clearCanvas();
+            }
+        }
+
+
+    }
