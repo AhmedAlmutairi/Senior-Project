2a26fd8edb236e3cda00f1e584807ea043e025cf
Whitney Meulink <wmeulink15@wou.edu>
2017-05-14 12:35:29 -0700

    adding signalR drawing finally

---
+++ myWallOfficial/myWall/myWall/DrawHub.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Microsoft.AspNet.SignalR;
+using Microsoft.AspNet.SignalR.Hubs;
+
+namespace myWall
+{
+    [HubName("drawHub")]
+    public class DrawHub : Hub
+    {
+        public void Move(int prevX, int prevY, int currX, int currY)
+        {
+            Clients.All.DrawFromHub(prevX, prevY, currX, currY);
+        }
+        public void DrawFromHub()
+        {
+            // Will be fired in 
+        }
+    }
+}
