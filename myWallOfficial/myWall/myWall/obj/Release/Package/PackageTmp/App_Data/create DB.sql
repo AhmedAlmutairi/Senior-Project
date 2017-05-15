cd05cd64a5709a3e40ac2108a0f2ea196c2662f1
CS460almutairi <aalmutairi15@wou.edu>
2017-05-15 15:32:39 -0700

    merge feature

--- myWallOfficial/myWall/myWall/obj/Release/Package/PackageTmp/App_Data/create DB.sql
+++
@@ -1,4 +0,0 @@
-﻿CREATE DATABASE [MyWallDB] ON
-PRIMARY (NAME = 'MyWallDB', FILENAME = 'C:\Users\Ahmed\Documents\engOrg\engorg\myWallOfficial\myWall\myWall\App_Data\MyWallDB.mdf')
-LOG ON (NAME = 'MyWallDB_log', FILENAME = 'C:\Users\Ahmed\Documents\engOrg\engorg\myWallOfficial\myWall\myWall\App_Data\MyWallDB_log.ldf')
-GO
