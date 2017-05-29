f64c6af0f18b64fff3d4ad1ea7aa6c7f76481ae8
DESKTOP-SVQ8TMB\torpy <pdwalla17@gmail.com>
2017-05-29 11:11:58 -0700

    cleaned up wall css, added question

--- myWallOfficial/myWall/myWall/Controllers/WallController.cs
+++ myWallOfficial/myWall/myWall/Controllers/WallController.cs
@@ -27,6 +27,8 @@ namespace myWall.Controllers
         private MyWallContext db = new MyWallContext();
         ApplicationDbContext d = new ApplicationDbContext();
 
+        /** Wall Routes **/
+
         [Route("Wall")]
         [HttpGet]
         public ActionResult Id( int? id)
@@ -37,15 +39,17 @@ namespace myWall.Controllers
             }
 
             List<object> myModel = new List<object>();
-            //var post = d.Walls.Find(id).Posts.ToList();
-            //myModel.Add(d.Walls.ToList());
-            //myModel.Add(d.Posts.ToList());
 
 
             var walls = from w in d.Walls
                        where w.Id == id
                        select w;
             var wall = walls.First();
+
+            var questions = from q in d.Posts
+                            join w in d.Walls on q.Id equals w.QuestionId
+                            where q.WallId == id
+                            select q;
 
             var posts = from p in d.Posts
                        join w in d.Walls on p.WallId equals w.Id
@@ -60,24 +64,7 @@ namespace myWall.Controllers
             myModel.Add(walls.ToList());
             myModel.Add(posts.ToList());
             myModel.Add(chats.ToList());
-            /* var content = d.Walls.
-            Join(d.Posts, u => u.Id, uir => uir.WallId,
-            (u, uir) => new { u, uir }).
-            Where(n => n.uir.WallId == n.u.Id)
-            .AsEnumerable().Select(m => new Post  //ContentViewModel
-            {
-                Id = m.uir.Id,
-                UserId = m.uir.UserId,
-                WallId = m.uir.WallId,
-                CallobId = m.uir.CallobId,
-                Title = m.uir.Title,
-                Image = m.uir.Image,
-                Contents = m.uir.Contents,
-                Description = m.uir.Description
-
-
-            }).ToList();*/
-            //return View(content);
+            myModel.Add(questions.ToList());
 
             return View(myModel);
         }
@@ -86,28 +73,6 @@ namespace myWall.Controllers
         {
             return RedirectToAction("Index", "Home");
         }
-
-        //// GET: Wall
-        //public ActionResult Index()
-        //{
-        //    var walls = db.Walls.Include(w => w.AspNetUsers);
-        //    return View(walls.ToList());
-        //}
-
-        //// GET: Wall/Details/5
-        //public ActionResult Details(int? id)
-        //{
-        //    if (id == null)
-        //    {
-        //        return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
-        //    }
-        //    Wall wall = db.Walls.Find(id);
-        //    if (wall == null)
-        //    {
-        //        return HttpNotFound();
-        //    }
-        //    return View(wall);
-        //}
 
         // GET: Wall/Create
         [HttpGet]
@@ -121,7 +86,6 @@ namespace myWall.Controllers
         public ActionResult Create(Wall wall)
         {
 
-
             //ApplicationUser currentUser = UserManager.FindById(User.Identity.GetUserId());
 
             if (User.Identity.IsAuthenticated)
@@ -134,103 +98,12 @@ namespace myWall.Controllers
 
                 d.SaveChanges();
 
-                return RedirectToAction("Comment", "Wall", new { id = wall.Id });
+                return RedirectToAction("Question", "Wall", new { id = wall.Id });
 
 
             }
             return View(wall);
         }
-
-        [HttpGet]
-        public ActionResult Comment( )
-        {
-            return View();
-        }
-
-        [HttpPost]
-        public ActionResult Comment(Post model, int id)
-        {
-
-            Wall Id = d.Walls.Find(id);
-
-            HttpPostedFileBase file = Request.Files["ImageData"];
-            //var UserId = User.Identity.GetUserId();
-            //model.UserId = User.Identity.GetUserId();
-            // model.WallId = 
-            //ContentRepository service = new ContentRepository();
-            int i = myWall(file, model, id);
-            if (i == 1)
-            {
-
-                return RedirectToAction("Id", "Wall", new { id = id });
-            }
-
-            return View(model);
-        }
-
-        public int myWall(HttpPostedFileBase file, Post contentViewModel, int id)
-        {
-            if (User.Identity.IsAuthenticated)
-            {
-
-                contentViewModel.Image = ConvertToBytes(file);
-                contentViewModel.UserId = User.Identity.GetUserId();
-                contentViewModel.WallId = id;
-                var Post = new Post
-                {
-
-                    UserId = contentViewModel.UserId,
-                    WallId = contentViewModel.WallId,
-                    Title = contentViewModel.Title,
-                    Description = contentViewModel.Description,
-                    Contents = contentViewModel.Contents,
-                    Image = contentViewModel.Image
-                };
-
-
-                d.Posts.Add(Post);
-                int i = d.SaveChanges();
-
-
-                if (i == 1)
-                {
-                    return 1;
-                }
-                else
-                {
-                    return 0;
-                }
-            }
-
-            return 0;
-
-        }
-
-        public byte[] ConvertToBytes(HttpPostedFileBase image)
-        {
-            byte[] imageBytes = null;
-            BinaryReader reader = new BinaryReader(image.InputStream);
-            imageBytes = reader.ReadBytes((int)image.ContentLength);
-            return imageBytes;
-        }
-
-        //// POST: Wall/Create
-        //// To protect from overposting attacks, please enable the specific properties you want to bind to, for 
-        //// more details see http://go.microsoft.com/fwlink/?LinkId=317598.
-        //[HttpPost]
-        //[ValidateAntiForgeryToken]
-        //public ActionResult Create([Bind(Include = "Id,Name,UserId")] Wall wall)
-        //{
-        //    if (ModelState.IsValid)
-        //    {
-        //        db.Walls.Add(wall);
-        //        db.SaveChanges();
-        //        return RedirectToAction("Index");
-        //    }
-
-        //    ViewBag.UserId = new SelectList(db.ApplicationUsers, "Id", "Question", wall.UserId);
-        //    return View(wall);
-        //}
 
         // GET: Wall/Edit/5
         public ActionResult Edit(int? id)
@@ -265,6 +138,70 @@ namespace myWall.Controllers
             return View(wall);
         }
 
+        // GET: Wall/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Wall wall = d.Walls.Find(id);
+            if (wall == null)
+            {
+                return HttpNotFound();
+            }
+            return View(wall);
+        }
+
+
+        // POST: Wall/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Wall wall = d.Walls.Find(id);
+            var posts = from p in d.Posts
+                        select p;
+
+            foreach (Post post in posts)
+            {
+                d.Posts.Remove(post);
+            }
+            d.Walls.Remove(wall);
+            d.SaveChanges();
+            return RedirectToAction("Index", "Home");
+        }
+
+        /** Comment Routes **/
+
+        [HttpGet]
+        public ActionResult Comment( )
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Comment(Post model, int id)
+        {
+
+            Wall Id = d.Walls.Find(id);
+
+            HttpPostedFileBase file = Request.Files["ImageData"];
+            //var UserId = User.Identity.GetUserId();
+            //model.UserId = User.Identity.GetUserId();
+            // model.WallId = 
+            //ContentRepository service = new ContentRepository();
+            int i = myWall(file, model, id);
+            if (i == 1)
+            {
+
+                return RedirectToAction("Id", "Wall", new { id = id });
+            }
+
+            return View(model);
+        }
+
+        [HttpGet]
         public ActionResult EditPost(int? id)
         {
             if (id == null)
@@ -302,6 +239,116 @@ namespace myWall.Controllers
             return View(post);
         }
 
+        public ActionResult DeletePost(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Post post = d.Posts.Find(id);
+            if (post == null)
+            {
+                return HttpNotFound();
+            }
+            return View(post);
+        }
+
+        [HttpPost, ActionName("DeletePost")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmedPost(int id)
+        {
+            Post post = d.Posts.Find(id);
+            int i = post.WallId;
+            d.Posts.Remove(post);
+            d.SaveChanges();
+            return RedirectToAction("Id", "Wall", new { id = i });
+        }
+
+        /** Question Routes **/
+        [HttpGet]
+        public ActionResult Question()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Question(Post model, int id)
+        {
+
+            Wall wall = d.Walls.Find(id);
+            //var qid = model.Id;
+            //wall.QuestionId = model.Id;
+            //d.SaveChanges();
+
+            HttpPostedFileBase file = Request.Files["ImageData"];
+            //var UserId = User.Identity.GetUserId();
+            //model.UserId = User.Identity.GetUserId();
+            // model.WallId = 
+            //ContentRepository service = new ContentRepository();
+            int i = myWall(file, model, id);
+            if ( i > 0)
+            {
+                wall.QuestionId = i;
+                d.SaveChanges();
+            }
+            //if (i == 1)
+            //{
+
+            //    return RedirectToAction("Id", "Wall", new { id = id });
+            //}
+            //return View(model);
+            return RedirectToAction("Id", "Wall", new { id = id });
+
+        }
+
+        /** Helper Methods **/
+
+        public int myWall(HttpPostedFileBase file, Post contentViewModel, int id)
+        {
+            if (User.Identity.IsAuthenticated)
+            {
+
+                contentViewModel.Image = ConvertToBytes(file);
+                contentViewModel.UserId = User.Identity.GetUserId();
+                contentViewModel.WallId = id;
+                var Post = new Post
+                {
+
+                    UserId = contentViewModel.UserId,
+                    WallId = contentViewModel.WallId,
+                    Title = contentViewModel.Title,
+                    Description = contentViewModel.Description,
+                    Contents = contentViewModel.Contents,
+                    Image = contentViewModel.Image
+                };
+
+
+                d.Posts.Add(Post);
+                int i = d.SaveChanges();
+
+
+                if (i == 1)
+                {
+                    return Post.Id;
+                }
+                else
+                {
+                    return 0;
+                }
+            }
+
+            return 0;
+
+        }
+
+        public byte[] ConvertToBytes(HttpPostedFileBase image)
+        {
+            byte[] imageBytes = null;
+            BinaryReader reader = new BinaryReader(image.InputStream);
+            imageBytes = reader.ReadBytes((int)image.ContentLength);
+            return imageBytes;
+        }
+
         public int myWallEdit(HttpPostedFileBase file, Post contentViewModel)
         {
             if (User.Identity.IsAuthenticated)
@@ -336,65 +383,6 @@ namespace myWall.Controllers
 
             return 0;
 
-        }
-
-        // GET: Wall/Delete/5
-        public ActionResult Delete(int? id)
-        {
-            if (id == null)
-            {
-                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
-            }
-            Wall wall = d.Walls.Find(id);
-            if (wall == null)
-            {
-                return HttpNotFound();
-            }
-            return View(wall);
-        }
-
-
-        // POST: Wall/Delete/5
-        [HttpPost, ActionName("Delete")]
-        [ValidateAntiForgeryToken]
-        public ActionResult DeleteConfirmed(int id)
-        {
-            Wall wall = d.Walls.Find(id);
-            var posts = from p in d.Posts
-                        select p;
-
-            foreach( Post post in posts)
-            {
-                d.Posts.Remove(post);
-            }
-            d.Walls.Remove(wall);
-            d.SaveChanges();
-            return RedirectToAction("Index", "Home");
-        }
-
-        public ActionResult DeletePost(int? id)
-        {
-            if (id == null)
-            {
-                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
-            }
-            Post post = d.Posts.Find(id);
-            if (post == null)
-            {
-                return HttpNotFound();
-            }
-            return View(post);
-        }
-
-        [HttpPost, ActionName("DeletePost")]
-        [ValidateAntiForgeryToken]
-        public ActionResult DeleteConfirmedPost(int id)
-        {
-            Post post = d.Posts.Find(id);
-            int i = post.WallId;
-            d.Posts.Remove(post);
-            d.SaveChanges();
-            return RedirectToAction("Id", "Wall", new { id = i });
         }
 
         protected override void Dispose(bool disposing)
